21c3f6c4efab7a7e71423b8b34846af088c7f121
KhanGeek <han1995.95@mail.ru>
2026-09-14 13:07:37 +0400

    Update JsonSerializer.cs

--- Assets/_Project/Develop/Utilites/DataManagment/Serializers/JsonSerializer.cs
+++ Assets/_Project/Develop/Utilites/DataManagment/Serializers/JsonSerializer.cs
@@ -6,12 +6,19 @@ namespace _Project.Develop
     {
         public string Serialize<TData>(TData data)
         {
-            return JsonConvert.SerializeObject(data);
+            return JsonConvert.SerializeObject(data, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Auto
+            });
         }
 
         public TData Deserialize<TData>(string serializedData)
         {
-            return JsonConvert.DeserializeObject<TData>(serializedData);
+            return JsonConvert.DeserializeObject<TData>(serializedData, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            });
         }
     }
 }
